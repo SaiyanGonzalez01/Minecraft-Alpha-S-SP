de001d91aab534fe591382728f903e599a2e8b11
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2025-04-14 21:56:24 -0400

    Update EntitySkinWalker.java

--- src/main/java/net/minecraft/src/EntitySkinWalker.java
+++ src/main/java/net/minecraft/src/EntitySkinWalker.java
@@ -3,7 +3,7 @@ package net.minecraft.src;
 public class EntitySkinWalker extends EntityMobs {
 	public EntitySkinWalker(World var1) {
 		super(var1);
-		this.texture = "/mob/char.png";
+		this.texture = "/mob/skinwalker.png";
 		this.field_9333_am = 0.5F;
 		this.field_762_e = 5;
 	}
