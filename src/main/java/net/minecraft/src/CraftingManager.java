9253c341b72ed5a2a922f48f6898629c1ef7c0c7
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2025-05-25 15:26:19 -0400

    Update CraftingManager.java

--- src/main/java/net/minecraft/src/CraftingManager.java
+++ src/main/java/net/minecraft/src/CraftingManager.java
@@ -65,6 +65,7 @@ public class CraftingManager {
 		this.addRecipe(new ItemStack(Item.goldbucketEmpty, 1), new Object[]{"# #", " # ", Character.valueOf('#'), Item.ingotGold});
 		this.addRecipe(new ItemStack(Item.dyeRed, 2), new Object[]{"#", Character.valueOf('#'), Block.plantRed});
 		this.addRecipe(new ItemStack(Item.dyeYellow, 2), new Object[]{"#", Character.valueOf('#'), Block.plantYellow});
+		this.addRecipe(new ItemStack(Block.planksRed, 1), new Object[]{"#X", "X#", Character.valueOf('#'), Item.dyeRed, Character.valueOf('#'), Block.planks});
 		Collections.sort(this.recipes, new RecipeSorter(this));
 		System.out.println(this.recipes.size() + " recipes");
 	}
